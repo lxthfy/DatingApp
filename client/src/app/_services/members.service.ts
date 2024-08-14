7197deae2d08f8c44b5e3cc243adcf1bd1ed8b6a
lxthfy <mohammadluthfy@gmail.com>
2024-08-14 20:03:24 +0700

    End of Section 9

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -2,30 +2,19 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Member } from '../_models/member';
-import { AccountService } from './account.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MembersService {
   private http = inject(HttpClient);
-  private accountService = inject(AccountService);
   baseurl = environment.apiUrl;
   
   getMembers() {
-    return this.http.get<Member[]>(this.baseurl + 'users', this.getHttpOptions());
+    return this.http.get<Member[]>(this.baseurl + 'users');
   }
 
   getMember(username: string) {
-    return this.http.get<Member>(this.baseurl + 'users/' + username, this.getHttpOptions());
-  }
-
-  getHttpOptions() {
-    return {
-      headers: new HttpHeaders({
-        Authorization: `Bearer ${this.accountService.currentUser()?.token}`
-      })
-    }
-
+    return this.http.get<Member>(this.baseurl + 'users/' + username);
   }
 }
